1d0aca8877a6dfd41085b1f8f34c3d4612e8e8b8
joaovmvini <joaopfvasconcelosx@outlook.com>
2021-11-17 06:09:55 -0300

    increase frames

--- desktop_control/server.js
+++ desktop_control/server.js
@@ -10,7 +10,7 @@ const io = new Server(server);
 const remoteSocket = ioc.connect('http://bd9d-177-12-101-74.ngrok.io', { reconnection: true });
 
 const ScreenSharer = require('./screen_sharer/sharer');
-const sharer = new ScreenSharer(remoteSocket, 1);
+const sharer = new ScreenSharer(remoteSocket, 5);
 
 const EventHandler = require('./event_handler/index');
 const eventHandler = new EventHandler(remoteSocket);
