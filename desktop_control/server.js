90394aad4c851ab632595d0211d299aad6843936
joaovmvini <joaopfvasconcelosx@outlook.com>
2021-11-17 06:13:23 -0300

    update frame

--- desktop_control/server.js
+++ desktop_control/server.js
@@ -10,7 +10,7 @@ const io = new Server(server);
 const remoteSocket = ioc.connect('http://bd9d-177-12-101-74.ngrok.io', { reconnection: true });
 
 const ScreenSharer = require('./screen_sharer/sharer');
-const sharer = new ScreenSharer(remoteSocket, 5);
+const sharer = new ScreenSharer(remoteSocket, 2);
 
 const EventHandler = require('./event_handler/index');
 const eventHandler = new EventHandler(remoteSocket);
